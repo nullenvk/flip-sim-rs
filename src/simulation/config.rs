94f068108f7f68b7c04651d8a82fd63622eca125
Gorek2137 <m04gorczyca@gmail.com>
2026-05-23 14:11:19 +0200

    config update

--- src/simulation/config.rs
+++ src/simulation/config.rs
@@ -2,4 +2,12 @@
 pub struct Config {
     pub width: usize,
     pub height: usize,
+    pub density: f64,
+    pub spacing: f64,
+    pub particleRadius: f64,
+    pub maxParticles: f64,
+    pub picFlipRaTio: f64,
+    pub gravity: f64,
+    pub dt: f64,
+    pub overRelaxation: f64
 }
